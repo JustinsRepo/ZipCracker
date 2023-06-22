d82812c44006cdf7cb389219e7a57b6f221663ac
Nolan Taft <killer123yo@gmail.com>
2023-06-21 20:01:43 -0600

    Update zipcracker.py

--- zipcracker.py
+++ zipcracker.py
@@ -1,10 +1,13 @@
 # imports
 import pyzipper
+import bcrypt
+import secrets
+from pathlib import Path
 import zlib
 import time
 import threading
 
-def open_password_protected_zip(file_path, password):
+def open_password_protected_zip(file_path, password, extraction_path):
     try:
         with pyzipper.AESZipFile(file_path, 'r') as zip_ref:
             # decrypt the zip file using the provided password
@@ -12,7 +15,7 @@ def open_password_protected_zip(file_path, password):
             # check if the zip file can be read successfully
             zip_ref.read(zip_ref.infolist()[0])
             # if no exception is raised, the password is correct
-            zip_ref.extractall(path=path_extract)
+            zip_ref.extractall(path=extraction_path)
             # if the password is correct, extract the contents of the zipfile
             return True
     except (pyzipper.BadZipFile, zlib.error):
@@ -22,10 +25,13 @@ def open_password_protected_zip(file_path, password):
 def crack_password(passwords):
     success = False
     for password in passwords:
-        if open_password_protected_zip(file_path, password):
-            success = True
-            print(f"\nPassword cracked successfully. Password is: {password}")
-            break
+        password_hash = bcrypt.haspw(password.encode(), bcrypt.gensalt(secrets.token_bytes(16)) # 16 bit salt generation
+        # For password hashing and verification
+        if bcrypt.checkpw(password.encode(), password_hash):
+            if open_password_protected_zip(file_path, password, extraction_path):
+                success = True
+                print(f"\nPassword cracked successfully. Password is: {password}")
+                break #optionally, just exit(1) would work here.
 
     if not success:
         print("\nUnable to crack the password. Password not found.")
@@ -38,17 +44,29 @@ banner = '\033[91m' + '''
     |  |     |  |     |  |     /  _____  \  |  |\   | 
     |__|     |__|     |__|    /__/     \__\ |__| \__| 
               By: PixelRazer & Trace
-    Contributers: JustinsRepo
+    Contributers: [JustinsRepo, KittenGirl]
 ''' + '\033[0m'
 
 print(banner)
 
 file_path = input("Enter the location of the zip file: ")
 password_list_file = input("Enter the location of the file containing the list of passwords: ")
-path_extract = input("Directory where you want to extract the contents of the files to: ")
+extraction_path = input("Directory where you want to extract the contents of the files to: ")
 
-with open(password_list_file, 'r', encoding='latin-1') as file:
-    passwords = [password.strip() for password in file.readlines()]
+file_path = Path(file_path).resolve(strict=True)
+password_list_file = Path(password_list_file).resolve(strict=True)
+extraction_path = Path(extraction_path).resolve()
+
+if not extraction_path.is_dir():
+    print("Invalid extraction path. Please provide a valid directory.")
+    exit(1)
+
+try:
+    with open(password_list_file, 'r', encoding='latin-1') as file:
+        passwords = [password.strip() for password in file.readlines()]
+except Exception as openZexception:
+    print(f"\nSomewhere goofed up: \n {openZexception}"+"\n"*10)
+    exit(0)
 
 print(f"\nCracking password...\n")
 
@@ -59,7 +77,7 @@ password_chunks = [passwords[i:i+num_threads] for i in range(0, len(passwords), 
 #thread creation
 threads = []
 for chunk in password_chunks:
-    thread = threading.Thread(target=crack_password, args=(chunk,))
+    thread = threading.Thread(target=crack_password, args=(zip_file_path,chunk,extraction_path))
     thread.start()
     threads.append(thread)
 
