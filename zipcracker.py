ddee8cf9270272db0a277b5466b829a6f1e4ad2f
zvrke <73403156+zvrke@users.noreply.github.com>
2024-06-18 22:02:56 -0600

    Update zipcracker.py

--- zipcracker.py
+++ zipcracker.py
@@ -1,36 +1,25 @@
-# imports
 import pyzipper
 import zlib
-import time
 import threading
 
-def open_password_protected_zip(file_path, password):
+def open_password_protected_zip(file_path, password, path_extract):
     try:
         with pyzipper.AESZipFile(file_path, 'r') as zip_ref:
-            # decrypt the zip file using the provided password
             zip_ref.setpassword(password.encode())
-            # check if the zip file can be read successfully
-            zip_ref.read(zip_ref.infolist()[0])
-            # if no exception is raised, the password is correct
             zip_ref.extractall(path=path_extract)
-            # if the password is correct, extract the contents of the zipfile
             return True
-    except (pyzipper.BadZipFile, zlib.error):
-        # if an exception is raised, the password is incorrect or the zip file is invalid
+    except (pyzipper.BadZipFile, zlib.error, RuntimeError, pyzipper.CryptoUnavailableError):
         return False
 
-def crack_password(passwords):
-    success = False
+def crack_password(file_path, passwords, path_extract):
     for password in passwords:
-        if open_password_protected_zip(file_path, password):
-            success = True
+        if open_password_protected_zip(file_path, password, path_extract):
             print(f"\nPassword cracked successfully. Password is: {password}")
-            break
+            return True
+    return False
 
-    if not success:
-        print("\nUnable to crack the password. Password not found.")
-
-banner = '\033[91m' + '''
+if __name__ == "__main__":
+    banner = '\033[91m' + '''
 .___________. __  .___________.    ___      .__   __. 
 |           ||  | |           |   /   \     |  \ |  | 
 `---|  |----`|  | `---|  |----`  /  ^  \    |   \|  | 
@@ -38,31 +27,32 @@ banner = '\033[91m' + '''
     |  |     |  |     |  |     /  _____  \  |  |\   | 
     |__|     |__|     |__|    /__/     \__\ |__| \__| 
               By: PixelRazer & Trace
-    Contributers: JustinsRepo
+    Contributers: zvrke
 ''' + '\033[0m'
+    print(banner)
 
-print(banner)
+    file_path = input("Enter the location of the zip file: ")
+    password_list_file = input("Enter the location of the file containing the list of passwords: ")
+    path_extract = input("Directory where you want to extract the contents of the files to: ")
 
-file_path = input("Enter the location of the zip file: ")
-password_list_file = input("Enter the location of the file containing the list of passwords: ")
-path_extract = input("Directory where you want to extract the contents of the files to: ")
+    with open(password_list_file, 'r', encoding='latin-1') as file:
+        passwords = [password.strip() for password in file.readlines()]
 
-with open(password_list_file, 'r', encoding='latin-1') as file:
-    passwords = [password.strip() for password in file.readlines()]
+    print(f"\nCracking password...\n")
 
-print(f"\nCracking password...\n")
+    # Splitting passwords into chunks for multi-threading
+    num_threads = 4
+    password_chunks = [passwords[i:i + num_threads] for i in range(0, len(passwords), num_threads)]
 
-#splitting password threads
-num_threads = 4
-password_chunks = [passwords[i:i+num_threads] for i in range(0, len(passwords), num_threads)]
+    # Thread creation
+    threads = []
+    for chunk in password_chunks:
+        thread = threading.Thread(target=crack_password, args=(file_path, chunk, path_extract))
+        thread.start()
+        threads.append(thread)
 
-#thread creation
-threads = []
-for chunk in password_chunks:
-    thread = threading.Thread(target=crack_password, args=(chunk,))
-    thread.start()
-    threads.append(thread)
+    # Thread joining
+    for thread in threads:
+        thread.join()
 
-#thread finish
-for thread in threads:
-    thread.join()
+    print("\nPassword cracking completed.")
